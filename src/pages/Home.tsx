8a71feba5afa67336dd1fe8762e0ef8a105f3aaa
tomkoom <tomash.sugint@gmail.com>
2024-10-14 18:34:47 +0700

    add tailwind, add shadcn/ui

---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+const Home = () => {
+  return (
+    <div className="">
+      <Button>Btn</Button>
+    </div>
+  );
+};
+
+export default Home;
