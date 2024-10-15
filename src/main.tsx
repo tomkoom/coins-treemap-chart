395789f621df05bbd793bc2bf26f92c4434713cf
tomkoom <tomash.sugint@gmail.com>
2024-10-15 08:28:07 +0700

    deploy to gh-pages

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,7 @@ import App from "./App.tsx";
 import "./index.css";
 
 // ...
-import {
-  QueryClient,
-  QueryClientProvider,
-  useQuery,
-} from "@tanstack/react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
 
--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/coins-treemap-chart/", // gh pages
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
